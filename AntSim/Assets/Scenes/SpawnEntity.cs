9c529ee17fbdea0c1bb1495510df1dbd3ba1d103
cfdUO <cfd@uoregon.edu>
2019-08-20 14:52:50 -0700

    Starter

---
+++ AntSim/Assets/Scenes/SpawnEntity.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEntity : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Entity;
+    [SerializeField]
+    private float xSpawn;
+    [SerializeField]
+    private float ySpawn;
+    private Vector2 position;
+
+    void spawn()
+    {
+            position = new Vector2(xSpawn, ySpawn);
+            Instantiate(Entity, position, transform.rotation);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            spawn();
+    }
+}
